7da7b7b27cc0f974c21d24d77aeb9643baa59d0c
TheMrRedstone <87324700+TheMrRedstone@users.noreply.github.com>
2021-12-07 14:37:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ $("#form").submit(function (e) {
     
     var API_KEY = '86d405d218ff05f29c075da984f5ca37'
     
-    var url = `https://api.serpstack.com/search?access_key=${API_KEY}&type=web&query=${query}`
+    var url = 'https://api.serpstack.com/search?access_key='+ API_KEY + "&type=web&query=" + query
+    console.log(url)
     
     $.get(url,function(data){
         console.log(data)
