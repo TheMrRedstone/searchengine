6b7012598e450d75b7dbb314101308175b48fd78
TheMrRedstone <87324700+TheMrRedstone@users.noreply.github.com>
2021-12-07 10:39:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $("#form").submit(function (e) {
-    e.preventDefault();
+    e.preventDefault()
     
-    var query = $("#search").val();
+    var query = $("#search").val()
     
     var AI_KEY = '86d405d218ff05f29c075da984f5ca37'
     
     var url = `http://api.serpstack.com/search?access_key=${API_KEY}&type=web&query=${query}`
     
     $.get(url,function(data){
-        console.log(data);
-    });
-});
+        console.log(data)
+    })
+})
