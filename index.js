2745d5c0e03e1b4f3c1498cbf457ba60a601f85d
TheMrRedstone <87324700+TheMrRedstone@users.noreply.github.com>
2021-12-08 18:13:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $("#form").submit(function (e) {
     
     var query = $("#search").val()
     
-    var API_KEY = '69da118144f72c8a5dad7b4028bf161940fd8b1a59efc5bab1ec88943791e79a'
+    var API_KEY = 'd8e5af2b153e1105fda032f5be5ad55976ac358c7594fde6a780888c29d23329'
     
     var url = 'https://serpapi.com/search.json?engine=google&q=' + query + '&api_key='+ API_KEY
     console.log(url)
